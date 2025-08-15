cc97e96f81aace7f507f84bf08e4c273351818b1
Kovvuri Nandi Vardhan Reddy <nandivardhanreddykovvuri2007@outlook.com>
2025-08-16 01:01:16 +0530

    Add files via upload

---
+++ Elephant.java
@@ -0,0 +1,14 @@
+//617A
+import java.util.Scanner;
+public class Elephant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = n/5;
+        if (n % 5 != 0) {
+            ans += 1;
+        }
+        
+        System.out.println(ans);
+    }
+}
